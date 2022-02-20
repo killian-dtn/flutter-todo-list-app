6eeb981dfe4db5a9ef24e0064df5e07c2a91ef2b
killian-dtn <killian.dantansentr@gmail.com>
2022-02-20 11:55:41 +0100

    TaksPreview done

--- lib/components/tasks/task_preview.dart
+++ lib/components/tasks/task_preview.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../models/task.dart';
@@ -9,21 +8,11 @@ class TaskPreview extends StatelessWidget {
   const TaskPreview({Key? key, required this.task}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Container(
-    child: RichText(
-      text: TextSpan(
-        children: [
-          WidgetSpan(
-            child: Icon(
-              task.completed ? Icons.task_alt : Icons.highlight_remove,
-              size: 14,
-              color: task.completed ? Colors.lightGreen : Colors.redAccent
-            )
-          ),
-          TextSpan(text: task.content),
-        ]
-      )
+  Widget build(BuildContext context) => ListTile(
+    title: Text(task.content),
+      leading: Icon(
+      task.completed ? Icons.task_alt : Icons.highlight_remove,
+      color: task.completed ? Colors.lightGreen : Colors.redAccent
     )
   );
-
 }
